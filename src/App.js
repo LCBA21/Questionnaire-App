194b056a9262efe16bf20c0ebd446e7e3339ecb9
LCBA21 <120941567+LCBA21@users.noreply.github.com>
2023-08-27 21:37:26 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
-import './App.css';
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
-// import Dashboard from './components/Dashboard';
-// import Create from './components/Create';
-// import Slider from './components/Slider';
-// import Questionnaire from './components/Questionnaire';
 import Create from './components/Create';
 import Slider from './components/Slider';
 import Questionnaire from './components/Questionnaire';
 import Dashboard from './components/Dashboard';
-// './components/images/OfficialLogo.png'
+import { useState } from 'react';
+
+
+
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+
   return (
     <div className="App">
-      <Router>
-      <Slider/>
+    <Router>
+      <Slider />
+      <Routes>
+        <Route
+          path='/'
+          element={<Dashboard todoList={todoList} />}
+        />
+        <Route
+          path='/create'
+          element={<Create todoList={todoList} setTodoList={setTodoList} />}
+        />
+        <Route
+          path='/question'
+          element={<Questionnaire todoList={todoList} />}
+        />
+      </Routes>
+    </Router>
+  </div>
 
-          <Routes>
-            <Route path='/' element={<Dashboard/>} />
-            <Route path='/create' element={<Create/>} />
-            <Route path='/question' element={<Questionnaire/>} />
-          </Routes>
-        </Router>
-
-    </div>
   );
 }
 
