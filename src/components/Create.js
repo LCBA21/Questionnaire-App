441cd79cf4b9c1bd15004ddf81c739737d785a20
LCBA21 <120941567+LCBA21@users.noreply.github.com>
2023-08-27 17:23:11 +0200

    Update Create.js

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,11 +1,75 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import {FaPen} from 'react-icons/fa'
+import {FaTrash} from 'react-icons/fa'
+
+
+
 
 const Create = () => {
-  return (
-    <div>
-             <h3>Create</h3>
-    </div>
-  )
-}
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState('');
 
-export default Create
+  useEffect(() => {
+    // Load stored tasks from localStorage on component mount
+    const storedTodoList = localStorage.getItem('todoList');
+    if (storedTodoList) {
+      setTodoList(JSON.parse(storedTodoList));
+    }
+  }, []);
+
+  useEffect(() => {
+    // Save updated tasks to localStorage whenever todoList changes
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+  }, [todoList]);
+
+  const handleChange = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  const addTask = () => {
+    const newTodoList = [...todoList, newTask];
+    setTodoList(newTodoList);
+    setNewTask(''); // Clear the input field
+  };
+
+  return (
+    <div className='div-center'>
+      <div >
+      <p className='text-header-question'>Create a Questionnaire</p>
+      </div>
+
+      <div>
+        <input className='input-question' value={newTask} onChange={handleChange} />
+      </div>
+
+      <div className='div-btn-add'>
+        <button className='btn-Add' onClick={addTask}>Add</button>
+      </div>
+
+      <div className='div-header-list'>
+      <p className='text-header-list'>List of Questions</p>
+      </div>
+
+
+      <div className=''>
+        {
+        
+        todoList.map((task, index) => {
+          return <div className='List' key={index}>{task} 
+          <FaPen
+          className='icons-create'
+
+          />
+          
+          <FaTrash
+          className='icons-create'
+
+          />
+          </div>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Create;
