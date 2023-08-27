8aced708b900c9b15a901cd5b434b49c099b1808
LCBA21 <120941567+LCBA21@users.noreply.github.com>
2023-08-27 17:24:01 +0200

    Update Questionnaire.js

--- src/components/Questionnaire.js
+++ src/components/Questionnaire.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import {Create} from './Create'
 
 
 const Questionnaire = () => {
   return (
-    <div>
-                  <h3>Questionnaire</h3>
+    <div className='div-center'>
+        <div className='div-container'>
+         
+          <div>
+            Item 2
+
+          <p className='text-header-question'>Questionnaire</p>
+          </div>
+          <div>Item 3</div>
+          <div>Item 4</div>
+
+          <div><button className='btn-submit'>Submit</button></div>
+        </div>
     </div>
   )
 }
