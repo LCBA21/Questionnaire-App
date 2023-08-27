f6f351234f032b6fb85ddaafd891237c4a55a13d
LCBA21 <120941567+LCBA21@users.noreply.github.com>
2023-08-27 21:41:57 +0200

    Update Questionnaire.js

--- src/components/Questionnaire.js
+++ src/components/Questionnaire.js
@@ -1,24 +1,30 @@
-import React from 'react'
-import {Create} from './Create'
+import React from 'react';
+import { FaPen, FaTrash } from 'react-icons/fa';
+import { useState } from 'react';
 
 
-const Questionnaire = () => {
+const Questionnaire = ({ todoList }) => {
   return (
     <div className='div-center'>
-        <div className='div-container'>
-         
-          <div>
-            Item 2
+      <div className='div-container'>
+        <p className='text-header-question'>Questionnaire</p>
 
-          <p className='text-header-question'>Questionnaire</p>
+
+          <div className=''>
+        {todoList.map((task, index) => (
+          <div className='List' key={index}>
+            {task}
+            <FaPen className='icons-create' />
+            <FaTrash className='icons-create'  />
           </div>
-          <div>Item 3</div>
-          <div>Item 4</div>
+        ))}
+      </div>
 
-          <div><button className='btn-submit'>Submit</button></div>
-        </div>
+
+        <button className='btn-submit'>Submit</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Questionnaire
+export default Questionnaire;
