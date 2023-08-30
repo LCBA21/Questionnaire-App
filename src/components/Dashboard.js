0992dc3e2eadba53df61a282d34350e53a91c45b
LCBA21 <120941567+LCBA21@users.noreply.github.com>
2023-08-30 16:06:48 +0200

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,16 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-const Dashboard = () => {
+const Dashboard = ({ agreenum, disagreenum, neutralnum }) => {
   return (
     <div>
-     <div className='div-stats-conatiner'>
-    <div className='stats'></div>
-    <div className='stats'></div>
-    <div className='stats'></div>
-    <div className='stats'></div>
-    </div>   
+      <div className='div-stats-conatiner'>
+        <div className='stats'>{agreenum} Agree</div>
+        <div className='stats'>{disagreenum} Disagree</div>
+        <div className='stats'>{neutralnum} Neutral</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
