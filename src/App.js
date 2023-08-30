86891d4ebdf0d7e068baa88f7938ef0e5c718246
LCBA21 <120941567+LCBA21@users.noreply.github.com>
2023-08-30 16:04:13 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,55 @@
-import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Create from './components/Create';
 import Slider from './components/Slider';
 import Questionnaire from './components/Questionnaire';
 import Dashboard from './components/Dashboard';
-import { useState } from 'react';
-
-
-
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const [selected,setSelected]=useState({})
+  const [agreenum, setAgreeNum] = useState(0);
+  const [disagreenum, setDisagreeNum] = useState(0);
+  const [neutralnum, setNeutralNum] = useState(0);
 
   return (
     <div className="App">
-    <Router>
-      <Slider />
-      <Routes>
-        <Route
-          path='/'
-          element={<Dashboard todoList={todoList} />}
-        />
-        <Route
-          path='/create'
-          element={<Create todoList={todoList} setTodoList={setTodoList} />}
-        />
-        <Route
-          path='/question'
-          element={<Questionnaire todoList={todoList} />}
-        />
-      </Routes>
-    </Router>
-  </div>
+      <Router>
+        <Slider />
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <Dashboard
+                agreenum={agreenum}
+                disagreenum={disagreenum}
+                neutralnum={neutralnum}
+              />
+            }
+          />
 
+          <Route
+            path='/create'
+            element={<Create todoList={todoList} setTodoList={setTodoList} />}
+          />
+
+          <Route
+            path='/question'
+            element={
+              <Questionnaire
+              todoList={todoList}
+              setAgreeNum={setAgreeNum}
+              setDisagreeNum={setDisagreeNum}
+              setNeutralNum={setNeutralNum}
+              selected={selected}
+              setSelected={setSelected} 
+
+              />
+            }
+          />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
