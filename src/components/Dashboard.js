d2975bb551b907149ed93674fbd7cc6835ae5e2e
LCBA21 <120941567+LCBA21@users.noreply.github.com>
2023-08-31 14:29:48 +0200

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,30 +1,37 @@
 import React from 'react';
-import { FaTurnUp } from 'react-icons/fa';
-import { FaTurnDown } from 'react-icons/fa';
-
-
-
 
 const Dashboard = ({ todoList,agreenum, disagreenum, neutralnum }) => {
   return (
     <div>
+
       <div className='div-stats-conatiner'>
         <div className='stats'>
         <div className='text-container-agree'>Agreements</div>
         <div className='text-size-count'>{agreenum}</div>
         <div></div>
-
         </div>
+
         <div className='stats'>
           <div className='text-container-disagree'>Disagreements</div>
           <div className='text-size-count'>{disagreenum}</div>
           <div></div>
           </div>
 
-        <div className='stats'>
+        <div className='stats-neutral'>
         <div className='text-container-neutral '>Neutral</div>
         <div className='text-size-neutral'> <p>{neutralnum}</p></div>
+        <div></div>
           </div>
+
+          <div className='stats-total'>
+          <div className='text-container-total'>Total</div>
+          {todoList.map((task, index) => (
+          <div className='List' key={index}>
+              {todoList.length}
+          </div>
+            ))}
+          </div>
+
       </div>
     </div>
   );
