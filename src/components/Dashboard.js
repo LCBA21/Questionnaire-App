399899658344aef6154371cdb7cd2d121642a4ba
LCBA21 <120941567+LCBA21@users.noreply.github.com>
2023-08-31 11:28:50 +0200

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { FaTurnUp } from 'react-icons/fa';
+import { FaTurnDown } from 'react-icons/fa';
 
-const Dashboard = ({ agreenum, disagreenum, neutralnum }) => {
+
+
+
+const Dashboard = ({ todoList,agreenum, disagreenum, neutralnum }) => {
   return (
     <div>
       <div className='div-stats-conatiner'>
-        <div className='stats'>{agreenum} Agree</div>
-        <div className='stats'>{disagreenum} Disagree</div>
-        <div className='stats'>{neutralnum} Neutral</div>
+        <div className='stats'>
+        <div className='text-container-agree'>Agreements</div>
+        <div className='text-size-count'>{agreenum}</div>
+        <div></div>
+
+        </div>
+        <div className='stats'>
+          <div className='text-container-disagree'>Disagreements</div>
+          <div className='text-size-count'>{disagreenum}</div>
+          <div></div>
+          </div>
+
+        <div className='stats'>
+        <div className='text-container-neutral '>Neutral</div>
+        <div className='text-size-neutral'> <p>{neutralnum}</p></div>
+          </div>
       </div>
     </div>
   );
